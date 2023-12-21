dc49a6eb233a9cf4fb2c15673e332a8ab06215d5
antonin-tarrade <antotarrade@gmail.com>
2023-12-21 10:28:37 +0100

    Parsing Csv et Affichage

---
+++ Projet-Graphe-Unity/Assets/Scripts/SatelliteManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SatelliteManager : MonoBehaviour
+{
+    public GameObject satellitePrefab;
+    public int csvToLoad;
+    private TextAsset[] allCsvs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allCsvs = Resources.LoadAll<TextAsset>("Csv");
+        DisplayFromCsv(csvToLoad);
+    }
+
+
+    private void DisplayFromCsv(int number){
+
+        string csv = allCsvs[number].text;
+
+        string[] lines = csv.Split('\n');
+
+        lines = lines.Skip(1).ToArray();
+
+        foreach(string l in lines){
+            string[] positions = l.Split(',');
+
+            GameObject satellite = Instantiate(satellitePrefab,transform);
+            
+            satellite.transform.position = new Vector3(float.Parse(positions[1])/5000,float.Parse(positions[2])/5000,float.Parse(positions[3])/5000);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
