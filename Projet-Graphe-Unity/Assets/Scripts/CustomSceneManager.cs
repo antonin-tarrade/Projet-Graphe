df8adacba61c4532ee6b6e2e2b5ded0afeba1228
Remoraki <gaga.gournay@gmail.com>
2024-01-26 11:37:33 +0100

    escape button

--- Projet-Graphe-Unity/Assets/Scripts/CustomSceneManager.cs
+++ Projet-Graphe-Unity/Assets/Scripts/CustomSceneManager.cs
@@ -8,14 +8,16 @@ public class CustomSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        
+        DontDestroyOnLoad(gameObject);      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
 
